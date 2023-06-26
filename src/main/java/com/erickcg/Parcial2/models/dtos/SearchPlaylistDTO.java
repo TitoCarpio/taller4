85228a85da41b36b55fe644916223439358610cb
TitoCarpio <00226620@uca.edu.sv>
2023-06-25 19:10:14 -0600

    agrego la version de spring

--- src/main/java/com/erickcg/Parcial2/models/dtos/SearchPlaylistDTO.java
+++ src/main/java/com/erickcg/Parcial2/models/dtos/SearchPlaylistDTO.java
@@ -1,7 +1,5 @@
 package com.erickcg.Parcial2.models.dtos;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
